96bd6c37d2ec770f8221df13400d6af49e719cf3
Martmists <martmists@gmail.com>
2020-09-04 22:23:44 +0200

    Update gradle

--- src/main/kotlin/io/github/gunpowder/mixin/plugin/SleepVoteModulePlugin.kt
+++ src/main/kotlin/io/github/gunpowder/mixin/plugin/SleepVoteModulePlugin.kt
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
 package io.github.gunpowder.mixin.plugin
 
 import org.objectweb.asm.tree.ClassNode
